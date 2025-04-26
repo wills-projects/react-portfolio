f5fc45776f7dd4b15c60a466d33b39c4b2f946e9
William <williampaullee@gmail.com>
2025-04-26 19:57:56 -0400

    fixed image track hidden content scroll problem

--- react-portfolio-app/src/components/ImageTrack.jsx
+++ react-portfolio-app/src/components/ImageTrack.jsx
@@ -18,18 +18,25 @@ function ImageTrack() {
       const maxDelta = window.innerWidth / 2;
     
       const percentage = (mouseDelta / maxDelta) * -100;
-      let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-      nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
+      let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage * 0.4;
+      // nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
+
+      // Subtract the VISIBLE width of the browser window from the TOTAL width of the track, which equals how much content is overflowing (how much we need to scroll)
+      // The track.scrollWidth when we divide tells us how much of the total width is hidden, so if we have 2000px of hidden content, we divide that by 3000px of total content and get 66% of hidden track
+      const maxMove = (track.scrollWidth - window.innerWidth) / track.scrollWidth * 100;
+
+      // Limit the movement of the image track between 0% and -maxMove%
+      nextPercentage = Math.max(Math.min(nextPercentage, 0), -maxMove);
       
       track.dataset.percentage = nextPercentage;
 
       track.animate({transform: `translate(${nextPercentage}%)`}, 
-                    {duration: 1200, fill:"forwards"});
+                    {duration: 300, fill:"forwards"});
 
       for (const image of track.getElementsByClassName("image")) 
       {
         image.animate({ objectPosition: `${100 + nextPercentage}% center` },
-                      { duration: 1200, fill: "forwards" });
+                      { duration: 300, fill: "forwards" });
       }
     };
 
@@ -57,14 +64,14 @@ function ImageTrack() {
   return (
     <div id="image-track-wrapper">
       <div id="image-track" ref={trackRef} data-mouse-down-at="0" data-prev-percentage="0">
-        <img src="./src/assets/valorant pixel art home.png" class="image" alt="A drawing I created using a system call graphics library." draggable="false" />
-        <img src="./src/assets/tartanhacks.jpg" class="image" alt="My friend and I judging a TartanHacks facial recognition project." draggable="false" />
-        <img src="./src/assets/jazz ensemble.jpg" class="image" alt="The Pitt Jazz Band ensemble rehearsing a song. I'm the vice president!" draggable="false" />
-        <img src="./src/assets/about me.jpg" class="image" alt="A photo of me exploring the Philadelphia Museum of Art!" draggable="false"/>
-        <img src="./src/assets/olympic national park.jpeg" class="image" alt="A photo of me staring out into Olympic National Park." draggable="false"/>
-        <img src="./src/assets/pittsburgh sunset.jpg" class="image" alt="A unique orange and purple sunset in Pittsburgh's Squirrel Hill." draggable="false"/>
-        <img src="./src/assets/avalon exchange thrift store.jpg" class="image" alt="The interior of Avalon Exchange thrift store in Pittsburgh." draggable="false"/>
-        <img src="./src/assets/website HTML progress.jpg" class="image" alt="The progression of my portfolio project's HTML code." draggable="false"/>
+        <img src="/valorant pixel art home.png" class="image" alt="A drawing I created using a system call graphics library." draggable="false" />
+        <img src="/tartanhacks.jpg" class="image" alt="My friend and I judging a TartanHacks facial recognition project." draggable="false" />
+        <img src="/jazz ensemble.jpg" class="image" alt="The Pitt Jazz Band ensemble rehearsing a song. I'm the vice president!" draggable="false" />
+        <img src="/about me.JPG" class="image" alt="A photo of me exploring the Philadelphia Museum of Art!" draggable="false"/>
+        <img src="/olympic national park.jpeg" class="image" alt="A photo of me staring out into Olympic National Park." draggable="false"/>
+        <img src="/pittsburgh sunset.jpg" class="image" alt="A unique orange and purple sunset in Pittsburgh's Squirrel Hill." draggable="false"/>
+        <img src="/avalon exchange thrift store.jpg" class="image" alt="The interior of Avalon Exchange thrift store in Pittsburgh." draggable="false"/>
+        <img src="/website HTML progress.jpg" class="image" alt="The progression of my portfolio project's HTML code." draggable="false"/>
       </div>
     </div>
   );
